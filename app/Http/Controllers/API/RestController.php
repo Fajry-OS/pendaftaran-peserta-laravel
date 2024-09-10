2383df33eea540060c26d46c74352d340042cd54
Fajry-OS <fajriseptiano@gmail.com>
2024-09-10 14:36:13 +0700

    first commit

---
+++ app/Http/Controllers/API/RestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Jurusan;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RestController extends Controller
+{
+    public function jurusan()
+    {
+        $jurusan = Jurusan::get();
+        return response()->json(['data' => $jurusan, 'message' => 'Data Jurusan'], 200);
+
+        //ini kalo belum bikin modelnya
+        // return response()->json("Data Jurusan");
+    }
+}
